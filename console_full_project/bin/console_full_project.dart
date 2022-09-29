615d498fa3a14ff67a051f1f55ced6d66abf8c99
Vivek Gupta <drguptavivek@yahoo.com>
2022-09-29 15:50:01 +0530

    asa

--- console_full_project/bin/console_full_project.dart
+++ console_full_project/bin/console_full_project.dart
@@ -3,3 +3,6 @@ import 'package:console_full_project/console_full_project.dart' as console_full_
 void main(List<String> arguments) {
   print('Hello world: ${console_full_project.calculate()}!');
 }
+
+
+cxvxcxv
